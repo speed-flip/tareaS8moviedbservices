f8bb0e0f6a3b79e911372c0ded0f29fe13332efa
vspeed21 <vtorresquintanilla0206@gmail.com>
2023-09-11 16:30:23 -0600

    add: showing movies list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MoviedbService } from './services/moviedb.service';
+import { Movie } from './services/moviedb.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tareaS8services';
+
+  movies: Movie[] = [];
+
+  gettingMovies: boolean = true;
+
+  constructor(private movieService: MoviedbService) { }
+
+  ngOnInit(): void {
+    this.movieService.getMovies().subscribe({
+      next: resp => {
+        console.log(resp.results);
+        this.movies = resp.results;
+        this.gettingMovies = false;
+      }
+    });
+  }
+
 }
